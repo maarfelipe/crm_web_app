11d9f59f9cc233acb262b307af16e712e2e2a04a
Matheus Fellipe_dell <matheus.maarfe@hotmail.com>
2023-10-13 20:07:39 -0300

    Added lead creation functionality

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import lead_list, lead_detail
+from .views import lead_list, lead_detail, lead_create
 
 urlpatterns = [
     path('', lead_list),
-    path('<pk>/', lead_detail),
+    path('create/', lead_create),
+    path('<int:pk>/', lead_detail),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from .models import Lead
+from .forms import LeadForm
+from .models import Agent, Lead
 
 
 def lead_list(request):
@@ -16,3 +17,29 @@ def lead_detail(request, pk):
         'lead': lead
     }
     return render(request, 'leads/lead_detail.html', context)
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == 'POST':
+        print('Receiving a post request')
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print('The form is valid')
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+
+            Lead.objects.create(
+                first_name = first_name,
+                last_name = last_name,
+                age = age,
+                agent = agent
+            )
+            print(f'{first_name} lead has been created')
+            return redirect('/leads')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'leads/lead_create.html', context)
