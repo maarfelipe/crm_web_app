538f7c4eac806c36b47eb1bdebe010fe3214048b
Matheus Fellipe_dell <matheus.maarfe@hotmail.com>
2023-10-13 20:22:49 -0300

    Improved lead creation using ModelForm

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 
-from .forms import LeadForm
-from .models import Agent, Lead
+from .forms import LeadModelForm
+from .models import Lead
 
 
 def lead_list(request):
@@ -11,6 +11,7 @@ def lead_list(request):
     }
     return render(request, 'leads/lead_list.html', context)
 
+
 def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
     context = {
@@ -18,25 +19,13 @@ def lead_detail(request, pk):
     }
     return render(request, 'leads/lead_detail.html', context)
 
-def lead_create(request):
-    form = LeadForm()
-    if request.method == 'POST':
-        print('Receiving a post request')
-        form = LeadForm(request.POST)
-        if form.is_valid():
-            print('The form is valid')
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
 
-            Lead.objects.create(
-                first_name = first_name,
-                last_name = last_name,
-                age = age,
-                agent = agent
-            )
-            print(f'{first_name} lead has been created')
+def lead_create(request):
+    form = LeadModelForm()
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
             return redirect('/leads')
 
     context = {
