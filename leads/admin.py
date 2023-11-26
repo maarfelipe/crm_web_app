a3899125e5fd9f31cc8d3b636b9cc53f3ab1109c
Matheus Felipe <matheus.maarfe@hotmail.com>
2023-11-26 17:22:21 -0300

    chore: restart crm_web_app project repository

--- leads/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Agent, Lead, User
-
-admin.site.register(User)
-admin.site.register(Lead)
-admin.site.register(Agent)
--- leads/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import lead_list, lead_detail, lead_create
-
-urlpatterns = [
-    path('', lead_list),
-    path('create/', lead_create),
-    path('<int:pk>/', lead_detail),
-]
